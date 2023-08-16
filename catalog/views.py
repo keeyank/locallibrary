24d67199f12f4c88c55a941e07899563fd2b4b73
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-08-16 12:47:01 -0700

    Part 5 Challenge

--- catalog/views.py
+++ catalog/views.py
@@ -14,11 +14,16 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_comedy = Genre.objects.filter(name__contains='Comedy').count()
+    num_unfunny_books = Book.objects.filter(genre__name__icontains='unfunny').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_comedy': num_comedy,
+        'num_unfunny_books': num_unfunny_books,
     }
 
     # Render the HTML template index.html with the data in the context variable
